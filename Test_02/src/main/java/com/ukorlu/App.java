978c5233a15deafdb203b07b858c6443d764f91d
unsalk <unsalk@OZUIT-UK.ozun.int>
2019-08-16 16:38:07 +0300

    test

--- Test_02/src/main/java/com/ukorlu/App.java
+++ Test_02/src/main/java/com/ukorlu/App.java
@@ -1,13 +1,22 @@
 package com.ukorlu;
 
+import com.ukorlu.utility.ConversionDto;
+import com.ukorlu.utility.FileUtil;
+import com.ukorlu.utility.JsonUtil;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+		String jsonFile = FileUtil.readFile("input.json");
+		ConversionDto conversion = JsonUtil.convertFromJson(jsonFile, ConversionDto.class);
+		StringBuilder builder = new StringBuilder();
+		builder.append(conversion.getField1() + ";");
+		builder.append(conversion.getField2() + ";");
+		builder.append(conversion.getField3() + ";");
+		FileUtil.writeFile("output.csv", builder.toString());
+	}
 }
