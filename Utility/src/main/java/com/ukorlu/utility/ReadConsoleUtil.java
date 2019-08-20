833c67566240386871915cf95cfe4b7c3eb568c1
unsalk <unsalk@OZUIT-UK.ozun.int>
2019-08-20 09:55:01 +0300

    test

--- Utility/src/main/java/com/ukorlu/utility/ReadConsoleUtil.java
+++ Utility/src/main/java/com/ukorlu/utility/ReadConsoleUtil.java
@@ -2,14 +2,25 @@ package com.ukorlu.utility;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class ReadConsoleUtil {
 
 	/**
 	 * reader.readLine();
-	 * 
 	 */
 	public static BufferedReader readConsole() {
 		return new BufferedReader(new InputStreamReader(System.in));
+	}
+
+	/**
+	 * in.nextLine();
+	 */
+	public static Scanner readConsoleWithScanner() {
+		return new Scanner(System.in);
+	}
+
+	public static String readLine() {
+		return System.console().readLine();
 	}
 }
