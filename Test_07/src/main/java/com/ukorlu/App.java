35f8192f075106907da9aa3e265c849603d5a270
unsalk <unsalk@OZUIT-UK.ozun.int>
2019-08-20 16:21:12 +0300

    test

--- Test_07/src/main/java/com/ukorlu/App.java
+++ Test_07/src/main/java/com/ukorlu/App.java
@@ -11,21 +11,31 @@ public class App {
 		String command = "";
 		while (!"exit".equalsIgnoreCase(command)) {
 			if (command.length() > 0) {
-				String parts[] = command.split("\\s");
-				if (parts.length == 2) {
-					if (parts[0].equalsIgnoreCase("trim")) {
-						System.out.println(StringUtil.trim(parts[1]));
-					} else {
-						System.out.println("Command Not Found!");
-					}
+				if ("help".equalsIgnoreCase(command)) {
+					printHelp();
 				} else {
-					System.out.println("Invalid Command!");
+					String parts[] = command.split("\\s");
+					if (parts.length == 2) {
+						if (parts[0].equalsIgnoreCase("trim")) {
+							System.out.println(StringUtil.trim(parts[1]));
+						} else if (parts[0].equalsIgnoreCase("trimAndSetNullIfBlank")) {
+							System.out.println(StringUtil.trimAndSetNullIfBlank(parts[1]));
+						} else {
+							System.out.println("Command Not Found!");
+						}
+					} else {
+						System.out.println("Invalid Command! For help write help command.");
+					}
 				}
-
 			}
 			Scanner scanner = ReadConsoleUtil.readConsoleWithScanner();
 			command = scanner.nextLine();
 		}
 		System.out.println("Program end!");
+	}
+
+	private static void printHelp() {
+		System.out.println("trim param : trim space from param");
+		System.out.println("trimAndSetNullIfBlank param : trim space if param blank return null");
 	}
 }
